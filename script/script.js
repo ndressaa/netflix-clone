236d22fcd58dd233075af6eed8afdd3311a86112
Andressa <andressajucoski@gmail.com>
2021-07-13 16:31:33 -0300

    tv shows section

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
 let tvShowsCarousel;
-let value = 0;
+let tvShowsNavValue = 0;
 
 window.onload = function () {
   tvShowsCarousel = document.getElementById('tv-shows-carousel');
 }
 
-function toLeft() {
-  if (tvShowsCarousel.style.left === '0px' || tvShowsCarousel.style.left === '') {
-    return;
+function tvShowsNavigation(event) {
+  if (event.target.className.includes('left')) {
+    if (tvShowsCarousel.style.left === '0px' || tvShowsCarousel.style.left === '') {
+      return;
+    }
+    tvShowsNavValue += 240;
+    tvShowsCarousel.style.left = `${tvShowsNavValue}px`;
   }
-  value += 240;
-  tvShowsCarousel.style.left = `${value}px`;
-}
-
-function toRight() {
-  if (tvShowsCarousel.style.left === '-480px') {
-    return;
+  if (event.target.className.includes('right')) {
+    if (tvShowsCarousel.style.left === '-480px') {
+      return;
+    }
+    tvShowsNavValue -= 240;
+    tvShowsCarousel.style.left = `${tvShowsNavValue}px`;
   }
-  value -= 240;
-  tvShowsCarousel.style.left = `${value}px`;
 }
-
