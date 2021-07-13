d01c32d66b390596d9bc19a20e5b939dd5a3b5ae
Andressa <andressajucoski@gmail.com>
2021-07-13 00:41:03 -0300

    bug fix

--- script/script.js
+++ script/script.js
@@ -6,18 +6,18 @@ window.onload = function () {
 }
 
 function toLeft() {
-  if (tvShowsCarousel.style.left === '-480px') {
-    return;
-  }
-  value -= 240;
-  tvShowsCarousel.style.left = `${value}px`;
-}
-
-function toRight() {
   if (tvShowsCarousel.style.left === '0px' || tvShowsCarousel.style.left === '') {
     return;
   }
   value += 240;
+  tvShowsCarousel.style.left = `${value}px`;
+}
+
+function toRight() {
+  if (tvShowsCarousel.style.left === '-480px') {
+    return;
+  }
+  value -= 240;
   tvShowsCarousel.style.left = `${value}px`;
 }
 
