e6a10e4d25c358ee6ba6cd5b89002a7ba89f3d61
Andressa <andressajucoski@gmail.com>
2021-07-13 00:30:38 -0300

    :sparkles: functional carousel  :sparkles:

---
+++ script/script.js
@@ -0,0 +1,23 @@
+let tvShowsCarousel;
+let value = 0;
+
+window.onload = function () {
+  tvShowsCarousel = document.getElementById('tv-shows-carousel');
+}
+
+function toLeft() {
+  if (tvShowsCarousel.style.left === '-480px') {
+    return;
+  }
+  value -= 240;
+  tvShowsCarousel.style.left = `${value}px`;
+}
+
+function toRight() {
+  if (tvShowsCarousel.style.left === '0px') {
+    return;
+  }
+  value += 240;
+  tvShowsCarousel.style.left = `${value}px`;
+}
+
